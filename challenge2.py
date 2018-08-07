24580f664a964edb4d8bca4ec23b67b15b514119
Prasad Rao Jammuna <40909002+PrasadRaoJ@users.noreply.github.com>
2018-08-07 22:53:23 +0530

    Update challenge2.py

--- challenge2.py
+++ challenge2.py
@@ -1,5 +1,5 @@
 """
-Question:
+Question 2:
 Write a program which can compute the factorial of a given numbers.
 The results should be printed in a comma-separated sequence on a single line.
 Suppose the following input is supplied to the program:
