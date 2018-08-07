c4253e51a906b43da0080ab06a2379dd82782c49
Prasad Rao Jammuna <40909002+PrasadRaoJ@users.noreply.github.com>
2018-08-07 20:22:42 +0300

    Solving Challenge 2

---
+++ challenge2.py
@@ -0,0 +1,24 @@
+"""
+Question:
+Write a program which can compute the factorial of a given numbers.
+The results should be printed in a comma-separated sequence on a single line.
+Suppose the following input is supplied to the program:
+8
+Then, the output should be:
+40320
+"""
+#-----------------------------------Method 1--------------------------------
+import math
+from math import factorial
+print(factorial(10))
+
+#-----------------------------------Method 2--------------------------------
+
+def fact(x):
+    f=1
+    for i in range(1,x+1):
+        f=f*i
+    return f
+
+res=fact(10)
+print(res)
